6cecb6b8ce614bb46a690729892c467eeb5fe53f
Balázs Cziráky <cziraky@users.noreply.github.com>
2021-04-15 20:23:14 +0200

    Updated sensor actions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-basic.show_icon(IconNames.HEART)
 lcd1602.set_address(lcd1602.I2C_ADDR.ADDR1)
 lcd1602.put_string("Keyestudio", 3, 0)
 lcd1602.set_LCD_Show(lcd1602.visibled.VISIBLE)
@@ -8,8 +7,25 @@ val = 0
 def on_forever():
     global val
     val += 1
+    if val > 1000:
+        val = 0 
     lcd1602.put_number(val, 7, 1)
     lcd1602.put_number(pins.analog_read_pin(AnalogPin.P0), 0, 1)
     lcd1602.put_number(pins.digital_read_pin(DigitalPin.P9), 12, 1)
-    basic.pause(1000)
+    if pins.digital_read_pin(DigitalPin.P9) == 1:
+        basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    else:
+        basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-basic.showIcon(IconNames.Heart)
 lcd1602.setAddress(lcd1602.I2C_ADDR.addr1)
 lcd1602.putString("Keyestudio", 3, 0)
 lcd1602.set_LCD_Show(lcd1602.visibled.visible)
@@ -7,8 +6,29 @@ let val = 0
 basic.forever(function on_forever() {
     
     val += 1
+    if (val > 1000) {
+        val = 0
+    }
+    
     lcd1602.putNumber(val, 7, 1)
     lcd1602.putNumber(pins.analogReadPin(AnalogPin.P0), 0, 1)
     lcd1602.putNumber(pins.digitalReadPin(DigitalPin.P9), 12, 1)
-    basic.pause(1000)
+    if (pins.digitalReadPin(DigitalPin.P9) == 1) {
+        basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    } else {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    }
+    
 })
